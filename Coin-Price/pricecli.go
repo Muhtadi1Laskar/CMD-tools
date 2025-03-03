a7c7a5f382bee4dbbe73cfdb0b475bc1639fa723
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-03-04 00:41:47 +0600

    Added the readJSON() function:

--- Coin-Price/pricecli.go
+++ Coin-Price/pricecli.go
@@ -1,8 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 )
+
+func readJSON(url string, target any) error {
+	response, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("unable to fetch data %v", err)
+	}
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return fmt.Errorf("error reading the response body: %v", err)
+	}
+
+	if err := json.Unmarshal(body, target); err != nil {
+		return fmt.Errorf("error reading the response body: %v", err)
+	}
+
+	return nil
+}
 
 func main() {
 	fmt.Println("Hello World")
